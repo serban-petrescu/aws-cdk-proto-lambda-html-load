c51bb932f0a43a5e9ba7821a5e589603141e9b59
Serban Petrescu <Serban.Petrescu@outlook.com>
2024-01-09 11:21:44 +0200

    Initial commit

---
+++ test/aws-cdk-proto-lambda-html-load.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkProtoLambdaHtmlLoad from '../lib/aws-cdk-proto-lambda-html-load-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-proto-lambda-html-load-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkProtoLambdaHtmlLoad.AwsCdkProtoLambdaHtmlLoadStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
